a8f4a4809b01b9cb93670dc86d4a9386c16148f1
MZM <49241786+Melkzedeck@users.noreply.github.com>
2019-09-27 10:05:39 -0300

    Update Movimentacao.h

--- Movimentacao.h
+++ Movimentacao.h
@@ -9,25 +9,22 @@ using std::to_string;
 
 class Movimentacao
 {
-        struct tm* dataMov;
+	//data no formato aaaa/mm/dd
+        string dataMov;
         string descricao;
         char debitoCredito;
         double valor;
     public:
         // Construtor com argumentos contendo descrição, tipo ('d' ou 'c') e o valor;
         Movimentacao(string, char, double);
-        //retorna data e hora da transação em formato struct tm (tipo da biblioteca ctime)
-        inline struct tm GetdataMov() { return *dataMov; }
+        //retorna data e hora da transação em formato aaaa/mm/dd
+        inline string GetdataMov() { return dataMov; }
         //retorna a descrição da movimentação
         inline string Getdescricao() { return descricao; }
         //retorna o tipo da movimentação ('c' para credito e 'd' para debito)
         inline char GetdebitoCredito() { return debitoCredito; }
         //retorna o valor da movimentação
         inline double Getvalor() { return valor; }
-        //retorna a data em string com a data no formato dd/mm/aaaa
-        string data();
-        //retorna a hora em string no formato hh:mm:ss
-        string hora();
 };
 
 #endif // MOVIMENTACAO_H
