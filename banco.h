d8c5de95b16dcec3a04c03f22ec44c29d166ca4a
IsadoraAQueiroz <55590504+IsadoraAQueiroz@users.noreply.github.com>
2019-09-29 17:57:06 -0300

    Add files via upload

--- banco.h
+++ banco.h
@@ -1,34 +1,37 @@
 #ifndef BANCO_H
 #define BANCO_H
-
+#include <list>
+#include <iostream>
+#include <fstream>
 #include <string>
 
 using std::string;
 
 class Banco{
 	public:
-		nomeBanco(string nome);
-		novoCliente();  //recebe de parametro cliente que é uma lista
-		novaConta();  //recebe de parametro cliente que é uma lista
-		excluiCliente();  //recebe parametro ou CPF ou CNPJ
-		excluiConta();  //recebe parametro numero conta
-		deposito();	//recebe de parametros numero conta, valor de deposito
-		saque();	//recebe de parametros numero conta, valor do saque
-		transferencia();	//recebe de parametros numero conta de origem, numero conta de destino,valor da tranferência
-		tarifa();
-		CPMF();
-		saldo();	//recebe parametro numero da conta;
-		extratoAtual();	//recebe parametro numero da conta;extrato do mes corrente
-		extratoEspecifico();	////recebe parametro numero da conta, e a data especifica;extrato a partir da data colocada
-		clientes();	//deve listar todos os clietes
-		contas();	//deve listar todos as contas
-		grava();	//vai gravar todos os dados em um arquivo
-		le();	//vai ler todos os dados de um arquivo
+		Banco(string nomeBanco);
+		void novoCliente(list<string> &Clientes;);  //recebe de parametro cliente que é uma lista
+		void novaConta(list<string> &Contas);  //recebe de parametro cliente que é uma lista
+		void excluiCliente(string cpf_cnpj);  //recebe parametro ou CPF ou CNPJ
+		void excluiConta(int numConta);  //recebe parametro numero conta
+		void deposito(int numConta,float deposito);	//recebe de parametros numero conta, valor de deposito
+		void saque(int numConta,float saque);	//recebe de parametros numero conta, valor do saque
+		void transferencia(int numContaOrigem, int numContaDestino,float transfeerencia);	//recebe de parametros numero conta de origem, numero conta de destino,valor da tranferência
+		void tarifa();
+		void CPMF();
+		void saldo(int numConta);	//recebe parametro numero da conta;
+		void extratoAtual(int numConta);	//recebe parametro numero da conta;extrato do mes corrente
+		void extratoEspecifico(int numConta,int data);	//recebe parametro numero da conta, e a data especifica; extrato a partir da data colocada
+		list<Banco> getClientes;	//deve listar todos os clietes ???
+		list<Banco> getContass;	//deve listar todos as contas  ???
+		le();
+		escreve();
 		
 	private:
 		string nomeBanco;
-		string* clientes;
-		string* contas;
+		list<string> Clientes;
+		list<string> Contas;
 		
 };
 #endif  // CONJUNTO_H
+
