9276b34e6ab719395d602e31c8d44eb875c47f7e
rafaeldores <45595657+rafaeldores@users.noreply.github.com>
2019-10-04 21:07:02 -0300

    Update banco.h

--- banco.h
+++ banco.h
@@ -4,39 +4,37 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-nomeCliente;
-cpf_cnpj;
-endereco;
-fone
+#include "conta.h"
 using std::string;
+using std::to_string;
 
 class Banco{
 	public:
-		Banco(string nomeBanco);
-		void novoCliente(Cliente& Cliente);  //recebe de parametro cliente
-		void novaConta(Cliente& Cliente);  //recebe de parametro cliente 
+		Banco(string nomebanco);
+		void novoCliente(Cliente& cliente);  //recebe de parametro cliente
+		void novaConta(Cliente& cliente);  //recebe de parametro cliente
 		void excluiCliente(string cpf_cnpj);  //recebe parametro ou CPF ou CNPJ
 		void excluiConta(int numConta);  //recebe parametro numero conta
 		void deposito(int numConta,float deposito);	//recebe de parametros numero conta, valor de deposito
 		void saque(int numConta,float saque);	//recebe de parametros numero conta, valor do saque
-		void transferencia(int numContaOrigem, int numContaDestino,float transfeerencia);	//recebe de parametros numero conta de origem, numero conta de destino,valor da tranferência
+		void transferencia(int numContaOrigem, int numContaDestino,float transfeerencia);	//recebe de parametros numero conta de origem, numero conta de destino,valor da tranferï¿½ncia
 		void tarifa();
 		void CPMF();
-		void saldo(int numConta);	//recebe parametro numero da conta;
+		double saldo(int numConta);	//recebe parametro numero da conta;
 		void extratoAtual(int numConta);	//recebe parametro numero da conta;extrato do mes corrente
 		void extratoEspecifico(int numConta,int data);	//recebe parametro numero da conta, e a data especifica; extrato a partir da data colocada
-		list<Banco> getClientes;	//deve listar todos os clietes ???
-		list<Banco> getContass;	//deve listar todos as contas  ???
-		le();
-		escreve();
-		
+		list<Cliente> getClientes;	//deve listar todos os clietes ???
+		list<conta> getContass;	//deve listar todos as contas  ???
+		//void le();
+		void escreve(string x);
+
 	private:
-		friendly class Cliente;
-		friendly class conta;
+		friend class Cliente;
+		friend class conta;
 		string nomeBanco;
-		list<string> Clientes;
-		list<string> Contas;
-		
+		list<Cliente> Clientes;
+		list<conta> Contas;
+
 };
 #endif  // CONJUNTO_H
 
