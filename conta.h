f6efb0026ac727934d47e38245f777052df5c875
rafaeldores <45595657+rafaeldores@users.noreply.github.com>
2019-10-04 21:08:15 -0300

    Update conta.h

--- conta.h
+++ conta.h
@@ -11,17 +11,17 @@ using std::list;
 
 class conta{
   public:
-    numConta(string cliente); // inicializa um numero de conta
+    conta(Cliente cliente); // inicializa um numero de conta
     int getNumConta(); //obtem o valor do numero da conta
     double getSaldo(); //obtem o valor do saldo disponivel na conta
-    cliente getCliente(); //obtem o nome do cliente
+    Cliente getCliente(); //obtem o nome do cliente
     int static getProximoNumConta(); //obtem o proximo numero da conta
     list <Movimentacao> getMovimentacoes(); //obtem as movimentaÃ§Ãµes feitas na conta
     void debita(double d, string deb); //debita um valor, recebe como parametros o valor a ser debitado e a descriÃ§Ã£o
     void credita(double c, string cred); //credita um valor, recebe como parametros o valor a ser creditado e a descriÃ§Ã£o
-    double extratoDias(int inicio, int fim); //retorna as movimentaÃ§Ãµes ocorridas de uma data inicial atÃ© uma data final
-    double extratoAtual(int inicio); //retorna as movimentaÃ§Ãµes ocorridas de uma data incial atÃ© a data atual
-    double extratoMes(); //retorna as movimentaÃ§Ãµes ocorridas no ultimo mes
+    list<Movimentacao> extratoDias(string inicio, string fim); //retorna as movimentaÃ§Ãµes ocorridas de uma data inicial atÃ© uma data final
+    list<Movimentacao> extratoAtual(string inicio); //retorna as movimentaÃ§Ãµes ocorridas de uma data incial atÃ© a data atual
+    list<Movimentacao> extratoMes(); //retorna as movimentaÃ§Ãµes ocorridas no ultimo mes
 
   private:
     int numConta;
@@ -29,8 +29,8 @@ class conta{
     Cliente cliente;
     int static proximoNumConta;
     list <Movimentacao> movimentacoes;
-    friendly class Cliente;
-    friendly class Movimentacao;
+    friend class Cliente;
+    friend class Movimentacao;
 
 };
 
