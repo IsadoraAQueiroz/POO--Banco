fe83063db625c19009cb0d01dd088bf329c6de3c
filipeer99 <48461022+filipeer99@users.noreply.github.com>
2019-10-05 10:54:40 -0300

    Update Cliente.h

--- Cliente.h
+++ Cliente.h
@@ -11,18 +11,27 @@ class Cliente
         string endereco;
         string fone;
     public:
+        //construtor padrão da classe
         Cliente();
-        Cliente(string nome, string documento, string logradouro, string telefone);
-        Cliente(string nome);
-        Cliente(string nome, string documento);
+        //inicialiiza um objeto cliente definindo os 4 atributos
+        Cliente(string nome, string documento, string logradouro,string fone); 
+        //permite acesso ao atributo nome
         inline string GetnomeCliente() { return nomeCliente; }
+        //altera atributo nome
         inline void SetnomeCliente(string val) { nomeCliente = val; }
+        //permite acesso ao atributo cpf/cnpj
         inline string Getcpf_cnpj() { return cpf_cnpj; }
+        //altera atriubto cpf/cnpj
         inline void Setcpf_cnpj(string val) { cpf_cnpj = val; }
+        //permite acesso ao atributo endereço
         inline string Getendereco() { return endereco; }
+        //altera atributo endereço
         inline void Setendereco(string val) { endereco = val; }
+        //permite acesso ao atributo fone
         inline string Getfone() { return fone; }
+        //altera atributo fone
         inline void Setfone(string val) { fone = val; }
+        //retorna se o cliente corrente é igual ao parâmetro
         bool operator!= (Cliente);
 };
 
